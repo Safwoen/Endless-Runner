33b2eeb6f9b6e85e2a65d68da8c64244805d25e0
Safwoen <113114216+Safwoen@users.noreply.github.com>
2022-09-22 15:31:11 +0100

    Set up project

---
+++ Assets/Playercontroller.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public GameObject groundchecker;
+    public LayerMask whatIsGround;
+    public float jumpforce = 150f;
+
+    
+    public float maxSpeed = 5.0f;
+    bool isOnGround = false;
+
+    // Create a reference to the Rigidbody2D so we can manipulate it
+    Rigidbody2D playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find the Rigidbody2D component that is attached to the same object as this script
+        playerObject = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Create a 'float' that will be equalto the player horizontal input
+        float movementValueX = Input.GetAxis("Horizontal");
+
+        //Change the X velocity of the Rigidbody2D to be equal to the movement value
+        playerObject.velocity = new Vector2(movementValueX * maxSpeed, playerObject.velocity.y);
+
+        //Check to see if the ground check object is touching the ground
+        isOnGround = Physics2D.OverlapCircle(groundchecker.transform.position, 0.000010f, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        {
+            playerObject.AddForce(new Vector2(0.0f, jumpforce));
+            Debug.Log("E");
+        }
+
+    }
+}
